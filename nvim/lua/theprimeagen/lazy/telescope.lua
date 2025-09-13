c7787f1874e80e1008ee177e77c92069ed54d7e0
parrvx <parrvx@gmail.com>
2025-09-13 14:46:31 -0300

    zotero

--- nvim/lua/theprimeagen/lazy/telescope.lua
+++ nvim/lua/theprimeagen/lazy/telescope.lua
@@ -1,40 +1,53 @@
--- Substitua o conteúdo do seu arquivo por este:
-
+-- ~/.config/nvim/lua/theprimeagen/lazy/telescope.lua
 return {
-    "nvim-telescope/telescope.nvim",
-
-    -- ⭐ AJUSTE: A propriedade 'cmd' foi removida.
-    -- Ao remover a instrução de carregamento preguiçoso, garantimos
-    -- que o lazy.nvim carregará o Telescope durante a inicialização.
-    -- Desta forma, ele estará sempre disponível para uso imediato.
-
-    tag = "0.1.5",
-
-    dependencies = {
-        "nvim-lua/plenary.nvim"
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.5",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    -- Declara o plugin do Zotero e sua dependência aqui
+    {
+      "jmbuhr/telescope-zotero.nvim",
+      dependencies = { "kkharji/sqlite.lua" },
     },
+  },
 
-    config = function()
-        require('telescope').setup({})
+  config = function()
+    -- Configuração principal do Telescope
+    require("telescope").setup({
+      -- Configuração específica para a extensão zotero
+      extensions = {
+        zotero = {
+          -- Aponta para o seu diretório de dados do Zotero
+          zotero_path = os.getenv("HOME") .. "/Zotero",
+        },
+      },
+    })
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pr', builtin.oldfiles, { desc = "Arquivos Recentes" })
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<Leader>pg', builtin.live_grep, {
-            desc = '[F]ind by [G]rep' -- Descrição para o plugin which-key
-        })
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-    end
+    -- Carrega a extensão APÓS a configuração
+    require("telescope").load_extension("zotero")
+
+    -- Seus atalhos existentes
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>pr', builtin.oldfiles, { desc = "Arquivos Recentes" })
+    vim.keymap.set('n', '<leader>pws', function()
+        local word = vim.fn.expand("<cword>")
+        builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>pWs', function()
+        local word = vim.fn.expand("<cWORD>")
+        builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>ps', function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+    vim.keymap.set('n', '<Leader>pg', builtin.live_grep, {
+        desc = '[F]ind by [G]rep'
+    })
+    vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+    -- Atalho final para o Zotero
+    vim.keymap.set("n", "<leader>zc", "<cmd>Telescope zotero<cr>", { desc = "Zotero: Citações" })
+  end,
 }
